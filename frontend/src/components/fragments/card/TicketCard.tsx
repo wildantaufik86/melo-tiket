081fccb9c17c725750106ac64e0b1fe45285af59
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2025-09-25 20:36:18 +0700

    fix: issue frontend #2 - TicketSection.tsx

--- frontend/src/components/fragments/card/TicketCard.tsx
+++ frontend/src/components/fragments/card/TicketCard.tsx
@@ -3,8 +3,8 @@ import localFont from 'next/font/local';
 import Link from 'next/link';
 
 const brotherFont = localFont({
-  src: '../../../../public/fonts/BROTHER-Bold.otf'
-})
+  src: '../../../../public/fonts/BROTHER-Bold.otf',
+});
 
 type TicketCard = {
   id?: number;
@@ -25,14 +25,16 @@ export default function TicketCard({
   idTicket,
 }: TicketCardProps) {
   return (
-    <div className="bg-[url(/images/bg-ticket.webp)] bg-contain bg-center bg-no-repeat aspect-2/3 flex justify-center items-center w-full max-w-[150px] md:max-w-[180px] lg:max-w-[200px] hover:scale-110 duration-200 ease-in-out">
-      <div className="flex flex-col items-center gap-4 py-4">
-        <h3 className={`font-semibold text-lg text-wrap w-[70%] text-center lg:text-3xl ${brotherFont.className}`}>
+    <div className="bg-[url(/images/bg-ticket.webp)] bg-contain bg-center bg-no-repeat aspect-2/3 flex justify-center items-center w-full max-w-[180px] sm:max-w-[180px] lg:max-w-[200px] hover:scale-110 duration-200 ease-in-out">
+      <div className="flex flex-col items-center gap-4">
+        <h3
+          className={`font-black text-2xl text-wrap w-[70%] text-center sm:text-4xl lg:text-5xl ${brotherFont.className}`}
+        >
           {ticket.name || ''}
         </h3>
-        <p className="text-xs text-center flex flex-col lg:text-sm">
+        <p className="text-sm text-center flex flex-col sm:text-lg">
           IDR{' '}
-          <span className="text-sm font-semibold lg:text-lg">
+          <span className="text-lg font-black sm:text-xl">
             {formattedPrice(ticket.price)}
           </span>
         </p>
@@ -40,7 +42,7 @@ export default function TicketCard({
           href={`/checkout/event/${idEvent}`}
           className="p-[1px] rounded-sm bg-gradient-to-r from-blue-500 via-purple-500 to-red-500"
         >
-          <div className="rounded-sm bg-secondary text-center text-xs py-2 px-4 lg:text-sm">
+          <div className="rounded-sm font-semibold bg-secondary text-center text-xs py-2 px-4 sm:text-sm">
             BUY TICKETS
           </div>
         </Link>
