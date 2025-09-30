216c9c9211b5eaadc734abe347526c2c3fa8fb0e
wildantaufik86 <wildantaufik86@gmail.com>
2025-09-30 23:36:27 +0700

    config: add rate limiter

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -5,11 +5,12 @@ import {
   refreshHandler,
   registerHandler,
 } from "../controllers/auth.controller";
+import authLimiter from "../utils/rateLimiter";
 
 const authRoutes = Router();
-authRoutes.post("/register", registerHandler);
-authRoutes.post("/login", loginHandler);
-authRoutes.post("/logout", logoutHandler);
-authRoutes.get("/refresh", refreshHandler);
+authRoutes.post("/register", authLimiter, registerHandler);
+authRoutes.post("/login", authLimiter, loginHandler);
+authRoutes.post("/logout", authLimiter, logoutHandler);
+authRoutes.get("/refresh", authLimiter, refreshHandler);
 
 export default authRoutes;
---
+++ backend/src/utils/rateLimiter.ts
@@ -0,0 +1,20 @@
+import rateLimit from "express-rate-limit";
+
+const authLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 10,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    error: "Terlalu banyak permintaan",
+    message: "Anda telah mencapai batas permintaan untuk endpoint ini. Silakan coba lagi setelah 10 menit."
+  },
+
+  handler: (req, res, next, options) => {
+    console.warn(`Rate limit terlampaui oleh IP: ${req.ip} pada endpoint: ${req.path}`);
+    res.status(options.statusCode).send(options.message);
+  },
+});
+
+export default authLimiter;
