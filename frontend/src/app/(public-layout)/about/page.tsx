d9361710f28391ead45f4933b5de0f6a676983f4
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2025-10-02 00:01:07 +0700

    add: about page

---
+++ frontend/src/app/(public-layout)/about/page.tsx
@@ -0,0 +1,121 @@
+import Label from '@/components/fragments/label/Label';
+
+const aboutMelofestContents: { text: string }[] = [
+  {
+    text: '• Melofest atau Melophile Festival merupakan sebuah acara yang unik yang menonjolkan konsep yang menciptakan pengalaman yang berbeda bagi para penonton. Festival ini juga menonjolkan kecintaan seseorang terhadap musik serta ingin mendapatkan euphoria dalam menyaksikan konser musik. ',
+  },
+  {
+    text: '• Selain itu, Melophile Festival memiliki unsur kejutan yang tak terduga dengan adanya aktifitas social berupa Donor Darah, dan Santunan Disabilita. Melalui konsep-konsep inovatif ini, Melophile Festival di Lhokseumawe berharap dapat memberikan pengalaman yang tidak hanya menghibur namun juga memberikan perhatian terhadap kepedulian terhadap aksi social.',
+  },
+  {
+    text: '• Harapannya menjadi pionir Festival Musik dengan pengalaman terbaik di Kota Lhokseumawe, menciptakan momen tak terlupakan bagi setiap penonton. Kami berkomitmen untuk menghadirkan inovasi dan keunikan dalam penyelenggaraan festival, menjadikan Melophile Festival sebagai destinasi utama bagi pecinta seni, musik, budaya, kuliner dan menjadi destinasi yang dinantikan setiap tahunnya juga sumber inspirasi bagi festival-festival lain di seluruh Indonesia khususnya Aceh.',
+  },
+];
+
+export const ticketTerms: { id: number; text: string }[] = [
+  {
+    id: 1,
+    text: 'Tiket hanya dapat dibeli melalui melophilefestival.com',
+  },
+  {
+    id: 2,
+    text: 'Pembelian tiket selain melalui melophilefestival.com TIDAK MENJADI TANGGUNGJAWAB PROMOTOR & segala resiko yang timbul menjadi tanggung jawab pembeli tiket.',
+  },
+  {
+    id: 3,
+    text: 'Harga belum termasuk pajak dan biaya layanan.',
+  },
+  {
+    id: 4,
+    text: 'Tiket yang sudah dibeli TIDAK DAPAT DIKEMBALIKAN dengan alasan apapun, termasuk dikarenakan perubahan jadwal dan/atau susunan acara dan/atau diakibatkan oleh batal tampilnya artis pengisi acara. Kecuali dikarenakan kesalahan/inisiatif dari PROMOTOR/EO.',
+  },
+  {
+    id: 5,
+    text: 'Tiket tidak bisa di-refund apabila terjadi kondisi kahar seperti: gempa bumi, banjir, tsunami, serta meninggal Pemuka Agama/Ulama Terkhususnya di Aceh yang menyebabkan batalnya acara.',
+  },
+  {
+    id: 6,
+    text: 'Pada saat proses pembelian tiket, pastikan mengisi SESUAI dengan identitas diri pembeli.',
+  },
+  {
+    id: 7,
+    text: 'Setelah pembelian tiket bakal mendapakatkan voucher e-tiket yang bakal ditukarkan pada periode waktu penukaran gelang atau hari-h.',
+  },
+  {
+    id: 8,
+    text: 'QR code pada e-tiket hanya bisa di scan sebanyak satu kali.',
+  },
+  {
+    id: 9,
+    text: 'Menunjukkan QR code yang tertera pada e-tiket dan identitas diri yang sesuai dengan data pada e-tiket.',
+  },
+  {
+    id: 10,
+    text: 'Promotor TIDAK AKAN membantu proses penukaran e-tiket menjadi gelang jika terdapat ketidaksesuaian data pada e-tiket dan identitas diri.',
+  },
+  {
+    id: 11,
+    text: 'Penukaran gelang yang diwakilkan atau oleh sebab pindah tangan kepemilikan WAJIB membawa surat kuasa dalam bentuk fisik dan bermaterai, yang ditandatangani pemberi dan penerima surat kuasa, serta melampirkan fotokopi KTP pemberi dan penerima surat kuasa.',
+  },
+  {
+    id: 12,
+    text: 'Anak dibawah usia 12 tahun WAJIB didampingi orang tua/wali dan keamanannya menjadi tanggung jawab orang tua/wali.',
+  },
+  {
+    id: 13,
+    text: 'Ibu hamil harap dalam pendampingan dan mengambil posisi yang dianggap paling aman, mengingat tata cahaya panggung dan suara dapat mengganggu kenyamanan bagi kehamilan, dan promotor tidak bertanggung jawab atas resiko apapun dalam hal ini.',
+  },
+  {
+    id: 14,
+    text: 'Gelang yang hilang/rusak tidak dapat digunakan dan menjadi tanggung jawab pemilik.',
+  },
+  {
+    id: 15,
+    text: 'Dengan bertransaksi dianggap telah membaca dan menyetujui syarat dan ketentuan ini.',
+  },
+];
+
+export default function AboutPage() {
+  return (
+    <div className="bg-[url(/images/dark-gradient.jpg)] bg-cover bg-center bg-no-repeat min-h-screen pt-28 pd-full flex flex-col gap-4">
+      <section className="flex flex-col mt-8 lg:mt-16">
+        <Label text="About Melofest" />
+        <div className="mt-8 bg-[url(/images/accessoris-bg.jpg)] bg-cover bg-center bg-no-repeat w-full p-4 border border-bg-primary rounded-sm flex flex-col gap-4 lg:mt-12">
+          {aboutMelofestContents.map((content, index) => (
+            <p key={index} className="text-sm text-justify lg:text-base">
+              {content.text}
+            </p>
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col mt-8 lg:mt-16">
+        <Label text="About TIcket" />
+        <div className="mt-8 bg-[url(/images/accessoris-bg.jpg)] bg-cover bg-center bg-no-repeat w-full p-4 border border-bg-primary rounded-sm flex flex-col gap-4 lg:mt-12">
+          {ticketTerms.map((content, index) => (
+            <p key={index} className="text-sm text-justify lg:text-base">
+              {`${index + 1} ${content.text}`}
+            </p>
+          ))}
+        </div>
+      </section>
+
+      {/* <section className="flex flex-col mt-8 lg:mt-16">
+        <Label text="Melofest History" />
+        <div className="mt-8 bg-[url(/images/accessoris-bg.jpg)] bg-cover bg-center bg-no-repeat w-full p-4 border border-bg-primary rounded-sm flex flex-col gap-4 lg:mt-12">
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 p-4">
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+            <div className="w-full h-[200px] bg-white"></div>
+          </div>
+        </div>
+      </section> */}
+    </div>
+  );
+}
